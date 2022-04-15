bb63d05b2568b29cee0aa0138eb4b517ae528515
Amaransh <90545826+Amaransh@users.noreply.github.com>
2022-04-15 20:00:26 +0530

    Add files via upload

---
+++ chatty.js
@@ -0,0 +1,7 @@
+function adduser()
+{
+    username = document.getElementById("username").value;
+    localStorage.setItem("username", username);
+
+    window.location = "chatt....y_chatt....y.html" 
+};
